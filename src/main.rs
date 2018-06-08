2c2b6f418aafc7e471cb8e7334e98f0ab59ecdc9
Nicholas Jones <dojoman19@gmail.com>
2018-06-07 21:36:55 -0400

    Stubbed handle_token and added test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
+#![feature(custom_derive)]
 
 extern crate rocket;
 
-#[get("/helloworld")]
-fn index() -> &'static str {
-        "Hello, world!"
+#[derive(FromForm)]
+struct PushCode {
+	code: String
+}
+
+#[get("/handle_token?<code>")]
+fn handle_token(code: PushCode) -> String {
+	code.code
 }
 
 fn main() {
-        rocket::ignite().mount("/", routes![index]).launch();
+	rocket::ignite().mount("/", routes![handle_token]).launch();
+}
+
+#[cfg(test)]
+mod tests{
+
+	extern crate rocket;
+	use rocket::local::Client;
+
+	#[test]
+	fn test_handle_token(){
+		let rocket = rocket::ignite().mount("/", routes![super::handle_token]);
+		let client = Client::new(rocket).unwrap();
+		let mut result = client.get("/handle_token?code=amfEasdksak").dispatch();
+		assert_eq!(result.body_string(), Some("amfEasdksak".into()));
+	}
 }
