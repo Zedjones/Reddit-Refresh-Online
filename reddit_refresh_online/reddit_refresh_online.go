b648e545935a2c0f34557b81024fad70a540a419
Zedjones <dojoman19@gmail.com>
2018-12-29 01:26:43 -0500

    Added functions to get email and user name

--- reddit_refresh_online/reddit_refresh_online.go
+++ reddit_refresh_online/reddit_refresh_online.go
@@ -10,6 +10,7 @@ import (
 )
 
 const ABOUT_URL = "https://www.reddit.com/%s/about.json"
+const USER_URL = "https://api.pushbullet.com/v2/users/me"
 
 func ValidateSub(sub string) bool {
 	if !strings.Contains(sub, "r/") {
@@ -33,4 +34,50 @@ func ValidateSub(sub string) bool {
 		return true
 	}
 	return false
+}
+
+func GetUserName(token string) string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", USER_URL, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating user request.")
+		return ""
+	}
+	req.SetBasicAuth(token, "")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error getting user response.")
+		return ""
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	var result map[string]interface{}
+	json.Unmarshal(body, &result)
+	name, ok := result["name"].(string)
+	if !ok {
+		return ""
+	}
+	return name
+}
+
+func GetEmail(token string) string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", USER_URL, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating user request.")
+		return ""
+	}
+	req.SetBasicAuth(token, "")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error getting user response.")
+		return ""
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	var result map[string]interface{}
+	json.Unmarshal(body, &result)
+	email, ok := result["email"].(string)
+	if !ok {
+		return ""
+	}
+	return email
 }
