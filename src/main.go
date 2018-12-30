6311ddcc729a34dc14841c18b8e963e74a842d6d
Zedjones <dojoman19@gmail.com>
2018-12-29 21:51:04 -0500

    Added route to handle OAuth for Pushbullet

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
@@ -13,18 +12,32 @@ type isValid struct {
 	IsValid bool `json:"is_valid"`
 }
 
+type userToken struct {
+	UserToken string `json:"user_token"`
+}
+
 func main() {
 	e := echo.New()
 	e.POST("/validate_subreddit", validateRoute)
+	e.POST("/handle_token", handleToken)
 	e.Start(":1234")
 }
 
 func validateRoute(c echo.Context) error {
 	sub := c.QueryParam("sub")
-	fmt.Println(sub)
 	if sub == "" {
 		return c.NoContent(http.StatusNotFound)
 	}
 	jsonBody := &isValid{reddit_refresh_online.ValidateSub(sub)}
+	return c.JSON(http.StatusOK, jsonBody)
+}
+
+func handleToken(c echo.Context) error {
+	code := c.QueryParam("code")
+	if code == "" {
+		return c.NoContent(http.StatusNotFound)
+	}
+	userTok := reddit_refresh_online.GetToken(code)
+	jsonBody := &userToken{userTok}
 	return c.JSON(http.StatusOK, jsonBody)
 }
