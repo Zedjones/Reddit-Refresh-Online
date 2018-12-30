be87f5f5714a70a834969e89c072e8daefd96156
Zedjones <dojoman19@gmail.com>
2018-12-29 20:48:40 -0500

    Added route to validate subreddits

--- src/main.go
+++ src/main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
 
 	"../reddit_refresh_online"
 )
 
+type isValid struct {
+	IsValid bool `json:"is_valid"`
+}
+
 func main() {
-	fmt.Println(reddit_refresh_online.ValidateSub("thisisamemeandfuckme"))
-	fmt.Println(reddit_refresh_online.ValidateSub("gamedeals"))
+	e := echo.New()
+	e.POST("/validate_subreddit", validateRoute)
+	e.Start(":1234")
+}
+
+func validateRoute(c echo.Context) error {
+	sub := c.QueryParam("sub")
+	fmt.Println(sub)
+	if sub == "" {
+		return c.NoContent(http.StatusNotFound)
+	}
+	jsonBody := &isValid{reddit_refresh_online.ValidateSub(sub)}
+	return c.JSON(http.StatusOK, jsonBody)
 }
